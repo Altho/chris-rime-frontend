b4a26eb2cfcad078421e6176f99a4bf5c0af9195
Altho <alan.at.thomas@gmail.com>
2022-04-20 23:22:40 +0200

    escaped

--- components/home/ShortBio.js
+++ components/home/ShortBio.js
@@ -16,7 +16,7 @@ export default function ShortBio() {
                 la batterie et surtout de la guitare et anime déja de nombreuses soirées privées. A 16 ans, il reçoit sa
                 première commande pour illustrer un court métrage puis rencontre Nguyen Lê avec qui il forme un duo qui
                 se produit de manière régulière dans les clubs de jazz parisiens. La majorité à peine acquise, il
-                traverse l'atlantique pour s'inscrire au mythique Berklee College of Music.
+                traverse l&aposatlantique pour s&aposinscrire au mythique Berklee College of Music.
             </Blockquote>
                     <div className={style.buttonContainer}>
                         <div className={style.bioButton}> <Link className={style.link} href={'/biography'}>{locale === 'en' ?('Read more') : ('En savoir plus')}</Link></div>
