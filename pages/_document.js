dae5830d70baca05765c8bf4eef6b3f8e85f0cf3
Altho <alan.at.thomas@gmail.com>
2022-04-21 22:51:37 +0200

    google font optimization

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,6 @@ class AppDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
                             <link
                                 href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@1,500&family=Nanum+Gothic&family=Orbitron&display=swap"
                                 rel="stylesheet" />
