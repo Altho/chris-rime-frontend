db6587c786d63f36fc9f1ea27b5f1f22f17f1cc3
Altho <alan.at.thomas@gmail.com>
2022-05-11 07:43:28 +0200

    changed photo page rendering method

--- pages/media/videos.js
+++ pages/media/videos.js
@@ -129,7 +129,7 @@ export async function getServerSideProps(ctx){
 
 
 
-        },
+        },revalidate: 10,
 
 
     }
--- pages/methods/[slug].js
+++ pages/methods/[slug].js
@@ -27,7 +27,7 @@ const useStyles = createStyles((theme) => ({
 }));
 
 
-export async function getServerSideProps({query, locale}, ctx) {
+export async function getStaticProps({query, locale}, ctx) {
     const jwt = parseCookies(ctx).jwt
    if(jwt){
        const methodData = await getMethodData(query.slug, locale)
@@ -114,7 +114,7 @@ export default function methodDetails({methodData}) {
             </div>
 
 
-            
+
 
 
         </Layout>
