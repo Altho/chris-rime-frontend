ed92137e0517aa32a5039a8544533874922b19c2
Altho <alan.at.thomas@gmail.com>
2022-05-11 23:29:54 +0200

    revamped buy button on album page

--- pages/albums/[slug].js
+++ pages/albums/[slug].js
@@ -2,15 +2,18 @@ import Layout from "../../components/layout";
 import {getAlbumData} from "../../libs/fetchAlbums";
 import AlbumInfos from "../../components/albums/AlbumInfos";
 import {MediaSeparator} from "../../components/albums/MediaSeparator";
+import {Cash} from 'tabler-icons-react';
 import {createStyles} from '@mantine/core';
 import Media from '../../components/albums/Media'
 import Listen from '../../components/albums/Listen'
 import styles from '../../styles/[slug].module.css'
-import {Image} from '@mantine/core'
+import Link from 'next/link'
+import {Image, Button, Modal} from '@mantine/core'
 import Reviews from "../../components/reviews";
 import {Menu, MenuButton, MenuItem} from "@szhsin/react-menu";
-import style from "../../styles/[slug].module.css";
+import '@szhsin/react-menu/dist/core.css';
 import {useRouter} from "next/router";
+import {useState} from "react";
 
 export async function getServerSideProps({locale, query}, ctx) {
 
@@ -23,7 +26,6 @@ export async function getServerSideProps({locale, query}, ctx) {
         password: process.env.DB_PASSWORD,
 
     };
-
 
 
     const login = await fetch(`${process.env.DB_HOST}/api/auth/local`, {
@@ -93,10 +95,25 @@ export default function AlbumDetails({albumData}) {
 
 function AlbumTitle({name, image, album, buy, digital}) {
     const {classes} = useStyles();
+    const [opened, setOpened] = useState(false);
 
     const locale = useRouter().locale;
     return (
         <div>
+            <Modal
+                centered
+                withCloseButton={false}
+                opened={opened}
+                onClose={() => setOpened(false)}
+            >
+                    <div className={styles.modalButtonContainer}>
+                        <a href={album.buy} target="_blank" rel={'noreferrer'}><Button className={styles.buyButton}>{locale==='en' ? 'BUY ON GUITAR4FANS (PHYSICAL)' : 'ACHETER SUR GUITAR4FANS (PHYSIQUE)'}</Button></a>
+                        <a href={album.digital} target="_blank" rel={'noreferrer'}> <Button className={styles.buyButton}>{locale==='en' ? 'BUY ON BANDCAMP (DIGITAL)' : 'ACHETER SUR BANDCAMP (NUMERIQUE)'}</Button></a>
+
+                    </div>
+
+
+            </Modal>
 
             <div className={styles.titleContainer}>
                 <div className={styles.albumHeader}>
@@ -119,11 +136,14 @@ function AlbumTitle({name, image, album, buy, digital}) {
                         <AlbumInfos release={album.date.toString()} label={album.label} artists={album.artistes}/>
                         <p className={styles.albumDescription}>{album.description}</p>
                         <div className={styles.buttonContainer}>
-                            <Menu menuButton={<MenuButton
-                                className={style.buy}>{locale === 'en' ? ('BUY') : ('ACHETER')}</MenuButton>}>
-                                <MenuItem className={styles.menuItem}>Guitar4Fan (CD)</MenuItem>
-                                <MenuItem className={styles.menuItem}>BandCamp (Digital)</MenuItem>
-                            </Menu>
+                            <Button leftIcon={< Cash/>}
+                                    className={styles.buy}
+                                    onClick={setOpened}
+
+                            >{locale === 'en' ? ('BUY') : ('ACHETER')}
+
+                            </Button>}>
+
                         </div>
                     </div>
 
