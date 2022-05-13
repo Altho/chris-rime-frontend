c19fff45f9c584dbe2fca1a479a68ec968cecadb
Altho <alan.at.thomas@gmail.com>
2022-05-13 12:10:23 +0200

    tweaked blog cards display

--- components/blog/BlogList.js
+++ components/blog/BlogList.js
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import styles from '../../styles/blogList.module.css'
-import { Card, Group, Text,Image, Button } from '@mantine/core';
+import { Card, Group, Text,Image, Button} from '@mantine/core';
 import Link from 'next/link'
 import {format} from 'date-fns'
 import {  fr } from 'date-fns/locale'
@@ -13,11 +13,16 @@ import {  fr } from 'date-fns/locale'
 export default function  blogList({title, publishedAt, content,locale, children,image,slug}){
     console.log(image)
 
+    let displayTitle = title.slice(0,25)
+    if(title.length > 25){
+        displayTitle = displayTitle+'...'
+    }
+
 
     return(
         <div className={styles.cardContainer}>
             <Link href={`/news/${slug}`}>
-        <Card shadow="sm" padding="lg" radius='0px' className={styles.card}>
+        <Card shadow="sm" padding="lg" radius='5px' className={styles.card}>
             <Card.Section>
                 <Image
                 src={`${image}`}
@@ -28,11 +33,10 @@ export default function  blogList({title, publishedAt, content,locale, children,
                 />
             </Card.Section>
 
-            <Group position="apart" style={{ marginBottom: 5, marginTop: 10 }}>
-                <Text weight={500} style={{color:'black', fontFamily:'Orbitron'}}>{title}</Text>
+            <Group  position="apart" style={{ marginBottom: 5, marginTop: 10 }}>
+                <Text  weight={500} style={{color:'black', fontFamily:'Orbitron', }}>{displayTitle}</Text>
 
             </Group>
-
             <Text size="sm" style={{  lineHeight: 1.5, overflow:'hidden' }}>
                 {content}
             </Text>
